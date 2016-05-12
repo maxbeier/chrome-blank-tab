67b16adb0e70fac3de26c8120dadb95150a2d68e
Max Beier <beierm@htw-berlin.de>
2016-05-12 11:16:31 +0200

    Added inter tab sync and, tabs to spaces and save function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const body = document.body;
 body.innerHTML = localStorage.getItem('notes') || '';
 body.onkeyup = () => localStorage.setItem('notes', body.innerHTML);
+
+// Inter Tab Synchronization
+window.onstorage = (event) => { body.innerHTML = event.newValue; };
+
+// Key Events
+document.onkeydown = (event) => {
+   // Change Tab to Spaces
+   if (event.code === 'Tab') {
+      event.preventDefault();
+      const spaces = '  ';
+      document.execCommand('insertText', false, spaces);
+   }
+
+   // Save per ⌘ + S
+   if (event.metaKey && event.key === 's') {
+      event.preventDefault();
+      const link = document.createElement('a');
+      link.download = 'notes.html';
+      link.href = `data:text/html,<!DOCTYPE html>\n${body.parentElement.outerHTML}`;
+      link.click();
+   }
+};
